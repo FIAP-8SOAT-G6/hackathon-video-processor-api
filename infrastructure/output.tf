6b8ed81ad93f42e9eb351f5af5940d3b87f103da
Gabriel <gabrielescodino@gmail.com>
2025-04-21 16:50:04 -0300

    Fix output.tf

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,5 +1,4 @@
-# output "invoke_url" {
-#   value = aws_api_gateway_deployment.deployment.invoke_url
-#   # value = "https://${aws_api_gateway_rest_api.api_gateway.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.prod_stage.stage_name}/"
-# }
-#
+output "invoke_url" {
+  value = aws_api_gateway_deployment.deployment.invoke_url
+  value = aws_api_gateway_deployment.deployment.invoke_url
+}
